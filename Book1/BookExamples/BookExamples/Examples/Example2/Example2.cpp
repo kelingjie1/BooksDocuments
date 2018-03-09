47bc73a38be3b82137ec8bfa75ce85f8aceb94c5
lingtonke <lingtonke@tencent.com>
2018-03-09 21:44:00 +0800

    更新

--- Book1/BookExamples/BookExamples/Examples/Example2/Example2.cpp
+++ Book1/BookExamples/BookExamples/Examples/Example2/Example2.cpp
@@ -47,18 +47,18 @@ void Example2::render()
     glUseProgram(program);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, vertice.data());
-//    //绘制点
-//    glDrawArrays(GL_POINTS, 0, vertice_num);
-//    //绘制线(0-1,2-3,4-5)
-//    glDrawArrays(GL_LINES, 0, vertice_num);
-//    //循环绘制线(0-1-2-3-4-5-0)
-//    glDrawArrays(GL_LINE_STRIP, 0, vertice_num);
-//    //连续绘制线(0-1-2-3-4-5)
-//    glDrawArrays(GL_LINE_LOOP, 0, vertice_num);
-//    //绘制三角形(0-1-2,3-4-5)
-//    glDrawArrays(GL_TRIANGLES, 0, vertice_num);
-//    //绘制连续三角形(0-1-2,1-2-3,2-3-4,3-4-5)
-//    glDrawArrays(GL_TRIANGLE_STRIP, 0, vertice_num);
-//    //绘制三角形扇形(0-1-2,0-2-3,0-3-4,0-4-5)
-//    glDrawArrays(GL_TRIANGLE_FAN, 0, vertice_num);
+    //绘制点
+    glDrawArrays(GL_POINTS, 0, vertice_num);
+    //绘制线(0-1,2-3,4-5)
+    glDrawArrays(GL_LINES, 0, vertice_num);
+    //循环绘制线(0-1-2-3-4-5-0)
+    glDrawArrays(GL_LINE_STRIP, 0, vertice_num);
+    //连续绘制线(0-1-2-3-4-5)
+    glDrawArrays(GL_LINE_LOOP, 0, vertice_num);
+    //绘制三角形(0-1-2,3-4-5)
+    glDrawArrays(GL_TRIANGLES, 0, vertice_num);
+    //绘制连续三角形(0-1-2,2-1-3,2-3-4,4-3-5)
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, vertice_num);
+    //绘制三角形扇形(0-1-2,0-2-3,0-3-4,0-4-5)
+    glDrawArrays(GL_TRIANGLE_FAN, 0, vertice_num);
 }
--- Book1/BookExamples/BookExamples/Examples/Resources/shader/Example2.vs
+++ Book1/BookExamples/BookExamples/Examples/Resources/shader/Example2.vs
@@ -1,9 +1,11 @@
+//声明shader的版本
 #version 300 es
-
+//定义shader中的变量
 layout(location = 0) in vec2 position;
-
 void main()
 {
+    //点的大小
     gl_PointSize = 20.;
+    //最终的位置
     gl_Position = vec4(position.x, position.y, 0., 1.);
 }
